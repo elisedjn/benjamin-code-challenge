f5619933435a744223b88f2e42eb3c0705afbff8
Elise Djn <elisedjn@protonmail.com>
2020-09-18 16:35:29 +0200

    animation

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ const bigCircle = document.getElementById("big-circle");
 const benjamin = document.querySelector(".benjamin");
 const wrapper = document.querySelector(".wrapper")
 
-wrapper.addEventListener("onClick", e => {
-  // let x = (e.clientX - (window.innerWidth/2));
-  bigCircle.style.backgroundColor = "red";
-  bigCircle.style.transform = `translate3D(50px, 0, 0)`;
-  smallCircle.style.transform = `translate3D(5px, 0, 0)`;
-  benjamin.style.transform = `translate3D(10px, 0, 0)`;
+wrapper.addEventListener("mousemove", e => {
+  let x = (e.clientX);
+  bigCircle.style.transform = `translate3D(${x/10}px, 0, 0)`;
+  smallCircle.style.transform = `translate3D(${-x/20}px, 0, 0)`;
+  benjamin.style.transform = `translate3D(${-x/35}px, 0, 0)`;
 
 })
